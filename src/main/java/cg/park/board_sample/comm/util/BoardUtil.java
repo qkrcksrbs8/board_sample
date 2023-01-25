bfd924b76689e3855005952337bb1902cf045915
pcg0902 <qkrcksrbs8@naver.com>
2023-01-25 09:43:58 +0900

    BoardUtil 구현

--- src/main/java/cg/park/board_sample/comm/util/BoardUtil.java
+++ src/main/java/cg/park/board_sample/comm/util/BoardUtil.java
@@ -5,15 +5,18 @@ import org.springframework.util.StringUtils;
 public final class BoardUtil {
 
     public static boolean isBlank(final CharSequence cs) {
-        final int strLen = cs.length();
-        if (strLen == 0) {
+        if (cs == null)
             return true;
-        }
+
+        final int strLen = cs.length();
+        if (strLen == 0)
+            return true;
+
         for (int i = 0; i < strLen; i++) {
-            if (!Character.isWhitespace(cs.charAt(i))) {
+            if (!Character.isWhitespace(cs.charAt(i)))
                 return false;
-            }
         }
+
         return true;
     }
 
