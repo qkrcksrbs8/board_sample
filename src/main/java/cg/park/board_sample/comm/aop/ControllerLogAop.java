91190f9e82bccec5e7ebfa9633cfc6e24150204d
pcg0902 <qkrcksrbs8@naver.com>
2023-02-06 09:41:25 +0900

    [AOP] 모든 Controller 진입 시 로그 적용

--- src/main/java/cg/park/board_sample/comm/aop/ControllerLogAop.java
+++ src/main/java/cg/park/board_sample/comm/aop/ControllerLogAop.java
@@ -1,5 +1,59 @@
 package cg.park.board_sample.comm.aop;
 
+import cg.park.board_sample.comm.util.BoardUtil;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Aspect
 public class ControllerLogAop {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //    @Around("execution(* cg.park.springlotto..controllers.*.*(..)) || execution(* cg.park.springlotto..services.*.*(..))")
+    @Around("execution(* cg.park.board_sample..controller.*.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        String type = joinPoint.getSignature().toShortString();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        logger.info("SSID = {}, ===================START===================", BoardUtil.requestedSessionId());
+        logger.info("SSID = {}, @Around : {}, param : {} ", BoardUtil.requestedSessionId(), type, BoardUtil.mapToStr(request.getParameterMap()));
+        return joinPoint.proceed();
+    }
+
+    //    @Before("execution(* cg.park.springlotto..controllers.*.*(..)) || execution(* cg.park.springlotto..services.*.*(..))")
+    @Before("execution(* cg.park.board_sample..controller.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        String type = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        if (args.length == 0 || args[0] instanceof HttpServletRequest){
+            logger.info("SSID = {}, @Before : {}, param : {}", BoardUtil.requestedSessionId(), type, BoardUtil.mapToStr(request.getParameterMap()));
+        }else{
+            logger.info("SSID = {}, @Before : {}, param : {}", BoardUtil.requestedSessionId(), type, BoardUtil.setParamParse(args[0].toString(), "="));
+        }
+    }
+
+    //    @AfterReturning(pointcut = "execution(* cg.park.springlotto..controllers.*.*(..)) || execution(* cg.park.springlotto..services.*.*(..))", returning="retValue")
+    @AfterReturning(pointcut = "execution(* cg.park.board_sample..controller.*.*(..))", returning="retValue")
+    public void after(JoinPoint joinPoint, Object retValue) {
+        String type = joinPoint.getSignature().toShortString();
+        logger.info("SSID = {}, @After : {}, result : {}", BoardUtil.requestedSessionId(), type, retValue);
+        logger.info("SSID = {}, ===================E N D===================", BoardUtil.requestedSessionId());
+    }
+
+    //    @AfterThrowing(pointcut = "execution(* cg.park.springlotto..controllers.*.*(..)) || execution(* cg.park.springlotto..services.*.*(..))", throwing = "ex")
+    @AfterThrowing(pointcut = "execution(* cg.park.board_sample..controller.*.*(..))", throwing = "ex")
+    public void afterThrowingAnException(JoinPoint joinPoint, Exception ex) {
+        logger.info("SSID = {}", BoardUtil.requestedSessionId());
+        logger.info("SSID = {}, ===================E N D===================", BoardUtil.requestedSessionId());
+    }
+
 }
--- src/main/java/cg/park/board_sample/comm/util/BoardUtil.java
+++ src/main/java/cg/park/board_sample/comm/util/BoardUtil.java
@@ -1,5 +1,12 @@
 package cg.park.board_sample.comm.util;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public final class BoardUtil {
 
     public static boolean isBlank(final CharSequence cs) {
@@ -15,6 +22,91 @@ public final class BoardUtil {
                 return false;
 
         return true;
+    }
+
+    public static String convertStr(String str) {
+        return (null == str) ? "" : str;
+    }
+
+    public static String requestedSessionId() {
+        return convertStr((((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest()).getRequestedSessionId());
+    }
+
+    public static String mapToStr(Map<String, String[]> map){
+        return (map == null || map.keySet().size() == 0)? "{null}":appendMapToStr(map);
+    }
+
+    public static String appendMapToStr(Map<String, String[]> map){
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        for (String key : map.keySet()) sb.append(strType(map.get(key), key)).append(", ");
+        sb.append("}");
+        return sb.toString().replace(", }", "}");
+    }
+
+    public static String strType (Object val, String key) {
+        boolean chkKey = false;
+        if (val == null) return key + "null";
+        if (val instanceof String) return keyVal(key, val, chkKey);
+        if (val instanceof String[]){
+            String[] arr = (String[]) val;
+            if(arr.length == 1) return keyVal(key, arr[0], chkKey);
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(arr).forEach(a -> sb.append(keyVal(key, a, chkKey)));
+        }
+        return keyVal(key, val, chkKey);
+    }
+
+    public static String keyVal(String key, Object obj, boolean chkKey) {
+        return (chkKey)? key+":"+obj.toString().replaceAll(".*", "****") : key+":"+obj;
+    }
+
+    public static String setParamParse(String str, String gubun) {
+        Map<String, String> map = new HashMap<String, String>();
+        if (isBlank(str)) return null;
+        if (str.contains("{")) return str;
+        str = str
+                .replace("{", "")
+                .replace("}", "");
+        String[] array = str.split(",");
+
+        for (int i = 0; i < array.length; i++) {
+            String[] param = array[i].split(gubun);
+            if (param.length > 1) {
+                map.put(param[0], param[1]);
+                continue;
+            }
+            map.put(param[0], "");
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (String key : map.keySet()) {
+            Object val = map.get(key);
+            key = key.trim();
+            boolean chkKey = false;
+            sb.append(loopVal(key, val, chkKey, gubun));
+            sb.append(keyVal2(key, val, chkKey, gubun));
+            sb.append(", ");
+        }
+        sb.append("}");
+        return sb.toString().replace(", }", "}");
+    }
+
+    public static String keyVal2(String key, Object obj, boolean chkKey, String gubun) {
+        return (chkKey)? key+gubun+obj.toString().replaceAll(".*", "****") : key+gubun+obj;
+    }
+
+    public static String loopVal(String key, Object val, boolean chkKey, String gubun){
+        if (val == null) return key + "null";
+        if (val instanceof String) return keyVal2(key, val, chkKey, gubun);
+        if (val instanceof String[]) {
+            String[] arr = (String[]) val;
+            if (arr.length == 1)  return keyVal2(key, arr[0], chkKey, gubun);
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(arr).forEach(a -> sb.append(keyVal2(key, a, chkKey, gubun)));
+            return sb.toString();
+        }
+        return null;
     }
 
 }
