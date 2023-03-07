03ad1d5b778ff43bcad2b3902f6fd542836088ed
pcg0902 <qkrcksrbs8@naver.com>
2023-03-07 09:25:15 +0900

    ResponseMav 변경

--- src/main/java/cg/park/board_sample/comm/util/ResponseMav.java
+++ src/main/java/cg/park/board_sample/comm/util/ResponseMav.java
@@ -1,5 +1,6 @@
 package cg.park.board_sample.comm.util;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,12 @@ public class ResponseMav extends ModelAndView {
     public ResponseMav(String viewName) {
         super.setViewName(viewName);
     }
+
+    public ResponseMav(String viewName, HttpStatus status) {
+        super.setViewName(viewName);
+        super.setStatus(status);
+    }
+
     public ResponseMav set(String key, Object obj) {
         super.addObject(key, obj);
         return this;
