ca1a26b3c4bfe4afc5a489488aff39694f438ada
pcg0902 <qkrcksrbs8@naver.com>
2023-03-30 09:09:42 +0900

    [HttpRequestHelper] 유틸 수정

--- src/main/java/cg/park/board_sample/comm/util/HttpRequestHelper.java
+++ src/main/java/cg/park/board_sample/comm/util/HttpRequestHelper.java
@@ -3,12 +3,18 @@ package cg.park.board_sample.comm.util;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class HttpRequestHelper {
 
     public static HttpServletRequest getCurrentRequest() {
         return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    }
+
+    public static HttpSession getCurrentSession() {
+        return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
     }
 
 }
