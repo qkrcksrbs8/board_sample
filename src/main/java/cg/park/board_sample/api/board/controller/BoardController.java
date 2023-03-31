8107d79dd3018266ea02d5288ee7dcf97e3d5ed7
pcg0902 <qkrcksrbs8@naver.com>
2023-03-31 09:38:04 +0900

    [Excel] 샘플 다운로드

--- src/main/java/cg/park/board_sample/api/board/controller/BoardController.java
+++ src/main/java/cg/park/board_sample/api/board/controller/BoardController.java
@@ -6,6 +6,8 @@ import cg.park.board_sample.comm.util.HttpRequestHelper;
 import cg.park.board_sample.comm.util.Message;
 import cg.park.board_sample.comm.util.PagingUtil;
 import cg.park.board_sample.comm.util.ResponseMav;
+import com.sun.rowset.internal.Row;
+import javafx.scene.control.Cell;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequestMapping("/board")
 @Controller
@@ -79,6 +83,57 @@ public class BoardController {
     @DeleteMapping("/{boardNo}")
     public ResponseEntity<Message> delete(@PathVariable("boardNo") Integer boardNo) {
         return new ResponseEntity<>(boardService.delete(boardNo) , HttpStatus.OK);
+    }
+
+    @GetMapping( "/excelSampleDown")
+    public void excelSampleDown(HttpServletResponse response) {
+
+//      import
+//        <dependency>
+//			<groupId>org.apache.poi</groupId>
+//			<artifactId>poi-ooxml</artifactId>
+//			<version>4.1.0</version>
+//		</dependency>
+
+//        SXSSFWorkbook workbook = new SXSSFWorkbook();
+//        workbook.setCompressTempFiles(true);
+//        SXSSFSheet sheet = workbook.createSheet();
+//        sheet.setRandomAccessWindowSize(100);
+//
+//        CellStyle headerStyle = workbook.createCellStyle();
+//        headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+//        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+//        headerStyle.setAlignment(HorizontalAlignment.CENTER);
+//
+//        CellStyle bodyStyle1 = workbook.createCellStyle();
+//        bodyStyle1.setAlignment(HorizontalAlignment.CENTER);
+//
+//        CellStyle bodyStyle2 = workbook.createCellStyle();
+//        bodyStyle2.setAlignment(HorizontalAlignment.LEFT);
+//
+//        Row headerRow = sheet.createRow(0);
+//
+//        String[] headerTexts = {"이름", "조직명", "주소", "전화번호"};
+//        int[] headerWidth = {4000, 6000, 8000, 6000};
+//        for (int i = 0; i < headerTexts.length; i++) {
+//            String text = headerTexts[i];
+//
+//            Cell cell = headerRow.createCell(i);
+//            cell.setCellStyle(headerStyle);
+//            cell.setCellValue(text);
+//
+//            sheet.setColumnWidth(i, headerWidth[i]);
+//        }
+//
+//        response.setContentType("ms-vnd/excel");
+//        response.setHeader("Content-Disposition", "attachment;filename=seller_management_sample.xlsx");
+//
+//        try {
+//            workbook.write(response.getOutputStream());
+//            workbook.close();
+//        } catch (IOException e) {
+//            logger.error(e);
+//        }
     }
 
 }
