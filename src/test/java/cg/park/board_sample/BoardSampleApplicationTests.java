74086bd0a13688534a4ee9bb3afe8f4e2f256832
pcg0902 <qkrcksrbs8@naver.com>
2023-05-09 11:20:53 +0900

    [enum] 테스트

--- src/test/java/cg/park/board_sample/BoardSampleApplicationTests.java
+++ src/test/java/cg/park/board_sample/BoardSampleApplicationTests.java
@@ -1,5 +1,6 @@
 package cg.park.board_sample;
 
+import cg.park.board_sample.comm.util.Stamp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class BoardSampleApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Stamp.F001.getCode();
+		Stamp.F001.getMessage();
 	}
 
 }
