7e0f62cf28517029dd51f5fd6efc17ab44bb6d14
pcg0902 <qkrcksrbs8@naver.com>
2023-05-19 16:11:01 +0900

    [ENUM] 코드 정의

--- src/main/java/cg/park/board_sample/comm/util/Stamp.java
+++ src/main/java/cg/park/board_sample/comm/util/Stamp.java
@@ -3,7 +3,8 @@ package cg.park.board_sample.comm.util;
 public enum Stamp {
     S001("S001", "정상 처리되었습니다."),
     F001("F001", "F로 시작하는 코드는 로직상 식별 가능한 오류"),
-    P001("P001", "P로 시작하는 코드는 파라미터 관련.")
+    P001("P001", "P로 시작하는 코드는 파라미터 관련."),
+    E001("E001", "E로 시작하는 코드는 오류.")
     ;
 
     private final String code;
