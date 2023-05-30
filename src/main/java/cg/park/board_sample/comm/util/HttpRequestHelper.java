eaccb81bfb3469b5a6c339f81070222ff32ffb8f
pcg0902 <qkrcksrbs8@naver.com>
2023-05-30 09:53:46 +0900

    [IP] 체크 도입

--- src/main/java/cg/park/board_sample/comm/util/HttpRequestHelper.java
+++ src/main/java/cg/park/board_sample/comm/util/HttpRequestHelper.java
@@ -17,4 +17,28 @@ public class HttpRequestHelper {
         return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
     }
 
+    public String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip != null && ip.indexOf(",") > -1) {
+            ip = ip.substring(0, ip.indexOf(","));
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 }
