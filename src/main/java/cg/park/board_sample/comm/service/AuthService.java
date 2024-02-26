aa594e0962efcfd0385b97aa25b20881579af2c5
pcg0902 <qkrcksrbs8@naver.com>
2024-02-26 09:28:30 +0900

    AuthService Value 어노테이션 제거

--- src/main/java/cg/park/board_sample/comm/service/AuthService.java
+++ src/main/java/cg/park/board_sample/comm/service/AuthService.java
@@ -7,6 +7,7 @@ import cg.park.board_sample.comm.util.MemberSession;
 import cg.park.board_sample.comm.util.Message;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
