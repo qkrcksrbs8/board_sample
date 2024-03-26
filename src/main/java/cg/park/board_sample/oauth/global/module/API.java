e059463092fd8b48a15aeb71f1115585a5eef341
pcg0902 <qkrcksrbs8@naver.com>
2024-03-26 09:35:49 +0900

    [oauth] req, res 추상 클래스 생성

---
+++ src/main/java/cg/park/board_sample/oauth/global/module/API.java
@@ -0,0 +1,12 @@
+package cg.park.board_sample.oauth.global.module;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+abstract public class API
+{
+    protected HttpServletRequest request;
+
+    protected HttpServletResponse response;
+
+}
