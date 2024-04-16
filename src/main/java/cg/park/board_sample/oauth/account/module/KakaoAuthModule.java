0e4c6a126d6749f6f96818433c25540b707971c9
pcg0902 <qkrcksrbs8@naver.com>
2024-04-16 09:44:59 +0900

    [oauth] Kakao 인증 모듈

---
+++ src/main/java/cg/park/board_sample/oauth/account/module/KakaoAuthModule.java
@@ -0,0 +1,178 @@
+package cg.park.board_sample.oauth.account.module;
+
+import cg.park.board_sample.oauth.account.bean.ApiKeyBean;
+import cg.park.board_sample.oauth.account.bean.UserInfoBean;
+import cg.park.board_sample.oauth.global.module.Util;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.builder.ServiceBuilderOAuth20;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.AccessTokenRequestParams;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class KakaoAuthModule extends AuthModule
+{
+    private static final String MODULE_NAME = "kakao";
+
+    private static final String API_KEY;
+    private static final String SECRET_KEY;
+    private static final String CALLBACK_URL;
+
+    static
+    {
+//        ApiKeyBean apiKeyBean = getApiKeyBean(MODULE_NAME);
+
+//        API_KEY = apiKeyBean.getApi();
+//        SECRET_KEY = apiKeyBean.getSecret();
+//        CALLBACK_URL = apiKeyBean.getCallback();
+        API_KEY="4f6a3f7fc682f73a86bc75ce8f003791";
+        SECRET_KEY="SECRET_KEY";
+        CALLBACK_URL="http://localhost:8080/oauth/callback";
+    }
+
+    private static final ServiceBuilderOAuth20 SERVICE_BUILDER = new ServiceBuilder(API_KEY).apiSecret(SECRET_KEY).callback(CALLBACK_URL);
+
+    private static final KakaoAuthModule INSTANCE = new KakaoAuthModule(SERVICE_BUILDER);
+
+    /**
+     * 생성자 메서드
+     *
+     * @param serviceBuilder: [ServiceBuilderOAuth20] API 서비스 빌더
+     */
+    private KakaoAuthModule(ServiceBuilderOAuth20 serviceBuilder)
+    {
+        super(serviceBuilder);
+    }
+
+    /**
+     * 인스턴스 반환 메서드
+     *
+     * @return [KakaoAuthModule] 인스턴스
+     */
+    public static KakaoAuthModule getInstance()
+    {
+        return INSTANCE;
+    }
+
+    /**
+     * 접근 토큰 반환 메서드
+     *
+     * @param code: [String] 인증 코드
+     *
+     * @return [OAuth2AccessToken] 접근 토큰
+     *
+     * @throws IOException 데이터 입출력 예외
+     */
+    @Override
+    public OAuth2AccessToken getAccessToken(String code) throws IOException, ExecutionException, InterruptedException
+    {
+        AccessTokenRequestParams params = new AccessTokenRequestParams(code);
+        params.addExtraParameter("client_id", API_KEY);
+        params.addExtraParameter("client_secret", SECRET_KEY);
+
+        return getAccessToken(params);
+    }
+
+    /**
+     * 유저 정보 객체 반환 메서드
+     *
+     * @param body: [String] OAuth 응답 내용
+     *
+     * @return [UserInfoBean] 유저 정보 객체
+     *
+     * @throws JsonProcessingException JSON 파싱 예외
+     */
+    @Override
+    public UserInfoBean getUserInfoBean(String body) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode node = mapper.readTree(body);
+
+        String email = node.get("kakao_account").get("email") == null ? "미동의" : node.get("kakao_account").get("email").textValue();
+        String name = node.get("kakao_account").get("profile").get("nickname") == null ? "미동의" : node.get("kakao_account").get("profile").get("nickname").textValue();
+        String picture = node.get("kakao_account").get("profile").get("profile_image_url") == null ? "/oauth2/assets/images/logo.png" : node.get("kakao_account").get("profile").get("profile_image_url").textValue();
+
+        return new UserInfoBean(email, name, picture, MODULE_NAME);
+    }
+
+    /**
+     * 연동 해제 결과 반환 메서드
+     *
+     * @param access: [String] 접근 토큰
+     *
+     * @return [boolean] 연동 해제 결과
+     *
+     * @throws IOException 데이터 입출력 예외
+     * @throws ExecutionException 실행 예외
+     * @throws InterruptedException 인터럽트 예외
+     */
+    @Override
+    public boolean deleteInfo(String access) throws IOException, ExecutionException, InterruptedException
+    {
+        OAuthRequest oAuthRequest = new OAuthRequest(Verb.POST, "https://kapi.kakao.com/v1/user/unlink");
+        oAuthRequest.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        oAuthRequest.addHeader("Authorization", Util.builder("Bearer ", access));
+
+        service.signRequest(access, oAuthRequest);
+
+        return service.execute(oAuthRequest).isSuccessful();
+    }
+
+    /**
+     * 정보 제공 동의 갱신 URL 반환 메서드
+     *
+     * @param state: [String] 고유 상태값
+     *
+     * @return [String] 정보 제공 동의 갱신 URL
+     */
+    @Override
+    public String getUpdateAuthorizationUrl(String state)
+    {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("state", state);
+        params.put("scope", "profile_nickname,profile_image,account_email");
+
+        return service.getAuthorizationUrl(params);
+    }
+
+    /**
+     * 접근 토큰 요청 URL 반환 메서드
+     *
+     * @return [String] 접근 토큰 요청 URL
+     */
+    @Override
+    public String getAccessTokenEndpoint()
+    {
+        return "https://kauth.kakao.com/oauth/token";
+    }
+
+    /**
+     * 인증 API 요청 URL 반환 메서드
+     *
+     * @return [String] 인증 API 요청 URL
+     */
+    @Override
+    protected String getAuthorizationBaseUrl()
+    {
+        return "https://kauth.kakao.com/oauth/authorize";
+    }
+
+    /**
+     * 사용자 정보 요청 URL 반환 메서드
+     *
+     * @return [String] 사용자 정보 요청 URL
+     */
+    @Override
+    protected String getUserInfoEndPoint()
+    {
+        return "https://kapi.kakao.com/v2/user/me";
+    }
+}
