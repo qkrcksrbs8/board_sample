33411884057c807bfe8ab43bebc944ca50b2f95e
pcg0902 <qkrcksrbs8@naver.com>
2024-04-26 14:04:34 +0900

    [oauth] 로그인 응답 bean

---
+++ src/main/java/cg/park/board_sample/oauth/account/bean/LoginResponseBean.java
@@ -0,0 +1,14 @@
+package cg.park.board_sample.oauth.account.bean;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/*
+ * 로그인 응답 객체 클래스
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class LoginResponseBean {
+}
